4318dcfdfb42619f5124e9e358ee476c81d4fff9
brendon tse, kira roberts and molly stambaugh <kira@>
2019-09-10 15:06:47 -0700

    add webpack build

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { pingPong } from './webpacktemplate'; //CHANGE TO CURRENT PROJECT/FILE NAMES
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+
+$(document).ready(function() {
+
+});
